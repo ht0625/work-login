65947c8a13dec66f853273cc200d819bb04ca5c4
hiroki tamura <hiirookii0625@gmail.com>
2020-08-26 17:30:31 +0900

    ログイン機能実装

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
+  root to: 'blogs#top'
+  resources :blogs
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
